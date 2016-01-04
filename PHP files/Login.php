73d07351fc2b80a7b8ca68f7c2875e087d06de45
Mohamed Ibrahim <mis1494@gmail.com>
2016-01-04 03:56:14 +0200

    Update Login.php

--- PHP files/Login.php
+++ PHP files/Login.php
@@ -2,7 +2,7 @@
 session_start() ;
 
 $response = array() ;
-if($_GET["logout"]==1 and $_SESSION['user_email'])
+if($_POST["logout"]==1 and $_SESSION['user_email'])
 {
     session_destroy() ; // closes all sessions
     $response["success"] = 0 ;
@@ -12,17 +12,17 @@ if($_GET["logout"]==1 and $_SESSION['user_email'])
 }else
 {
     include("Connection.php") ;
-    if($_GET['loginEmail'] && $_GET['loginPassword'])
+    if($_POST['loginEmail'] && $_POST['loginPassword'])
     {
-        $hashed =  md5(md5($_GET['loginEmail']).$_GET['loginPassword']) ;
-$query = "SELECT * FROM user WHERE user_email = '".mysqli_real_escape_string($link,$_GET['loginEmail'])."' AND password = '.$hashed.' LIMIT 1" ;
+        $hashed =  md5(md5($_POST['loginEmail']).$_POST['loginPassword']) ;
+$query = "SELECT * FROM user WHERE user_email = '".mysqli_real_escape_string($link,$_POST['loginEmail'])."' AND password = '.$hashed.' LIMIT 1" ;
         // LIMIT 1 is used to just check for one user
     
         $result = mysqli_query($link, $query) ;
         $row = mysqli_fetch_array($result) ;
         if($row) // if it exists(not null)
         {
-            $_SESSION['user_email'] = $_GET['loginEmail'] ;
+            $_SESSION['user_email'] = $_POST['loginEmail'] ;
             $response["success"] = 1 ;
             $response["message"] = $row["username"];
             // echoing JSON response
