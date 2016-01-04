7dbf20cefcb0ce93a6cd2577f6cd1dd9ce699654
Mohamed Ibrahim <mis1494@gmail.com>
2016-01-04 03:59:33 +0200

    Update Signup.php

--- PHP files/Signup.php
+++ PHP files/Signup.php
@@ -1,32 +1,32 @@
 <?php
 session_start() ;
 include("Connection.php") ; // include connection file
-    if(!$_GET['username'])
+    if(!$_POST['username'])
         $error .= "\nPlease enter a username" ;
 
-    if(!$_GET['email']) // email not entered
+    if(!$_POST['email']) // email not entered
         $error .= "\nPlease enter your email" ;
-    else if(!filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) // This checks if the given parameter is valid according
+    else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) // This checks if the given parameter is valid according
         $error .= "\nPlease enter a valid email address" ;           // to the second parameter(validate email)
 
-    if(!$_GET['password'])
+    if(!$_POST['password'])
         $error .= "\nPlease enter a password" ;
     else // we need to check that it is at least 8 chars. long and contains at least one capital case letter
     {
-        if(strlen($_GET['password'])<8)
+        if(strlen($_POST['password'])<8)
             $error .= "\nPlease enter a password at least 8 characters long" ;
-        if(!preg_match('`[A-Z]`', $_GET['password'])) // First parameter is a regex checking if there exits a letter from
+        if(!preg_match('`[A-Z]`', $_POST['password'])) // First parameter is a regex checking if there exits a letter from
             $error .= "\nPlease include at least one capital letter in your password" ; // A to Z in the second parameter
     }
 
-    if(!$_GET['securityQuestion'])
+    if(!$_POST['securityQuestion'])
         $error .= "\nPlease enter a security question" ;
-    if(!$_GET['securityAnswer'])
+    if(!$_POST['securityAnswer'])
         $error .= "\nPlease enter a security answer" ;
 
-    if(!$_GET['altEmail']) // email not entered
+    if(!$_POST['altEmail']) // email not entered
         $error .= "\nPlease enter an alternative email" ;
-    else if(!filter_var($_GET['altEmail'], FILTER_VALIDATE_EMAIL))
+    else if(!filter_var($_POST['altEmail'], FILTER_VALIDATE_EMAIL))
         $error .= "\nPlease enter a valid alternative email" ; 
               
     if($error)
@@ -38,9 +38,9 @@ include("Connection.php") ; // include connection file
     }else
     {
         $link = mysqli_connect("mysql1.000webhost.com", "a8139710_mis94", "12345hom", "a8139710_outdoor") ;
-        $query = "SELECT * FROM user WHERE user_email = '".mysqli_real_escape_string($link,$_GET['email'])."'" ;
+        $query = "SELECT * FROM user WHERE user_email = '".mysqli_real_escape_string($link,$_POST['email'])."'" ;
         
-        $hashed =  md5(md5($_GET['email']).$_GET['password']) ;
+        $hashed =  md5(md5($_POST['email']).$_POST['password']) ;
         $result = mysqli_query($link, $query) ; // go execute
 
         //if(!$result || mysqli_num_rows($result) == 0)
@@ -52,18 +52,18 @@ include("Connection.php") ; // include connection file
             echo json_encode($response) ;
         }else
         {
-            $username = $_GET['username'] ;
-            $securityQuestion = $_GET['securityQuestion'] ;
-            $securityAnswer = $_GET['securityAnswer'] ;
-            $altEmail = $_GET['altEmail'] ;
-            $query = "INSERT INTO user (user_email, username, password, security_question, security_answer, alternative_email) VALUES ('".mysqli_real_escape_string($link,$_GET['email'])."', '$username', '.$hashed.', '$securityQuestion', '$securityAnswer', '$altEmail')" ;
+            $username = $_POST['username'] ;
+            $securityQuestion = $_POST['securityQuestion'] ;
+            $securityAnswer = $_POST['securityAnswer'] ;
+            $altEmail = $_POST['altEmail'] ;
+            $query = "INSERT INTO user (user_email, username, password, security_question, security_answer, alternative_email) VALUES ('".mysqli_real_escape_string($link,$_POST['email'])."', '$username', '.$hashed.', '$securityQuestion', '$securityAnswer', '$altEmail')" ;
             
             mysqli_query($link, $query) ; // go execute
             //echo "You've been signed up" ;
 // mysql_query("INSERT INTO user(user_email, username, password) VALUES('$email', '$username', '$password')");
             $response["success"] = 1;
             $response["message"] = $username;
-            $_SESSION['user_email'] = $_GET['email'] ;
+            $_SESSION['user_email'] = $_POST['email'] ;
             // echoing JSON response
             echo json_encode($response);
             //create a session variable
